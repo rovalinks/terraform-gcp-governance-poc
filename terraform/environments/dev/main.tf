28bb618743094817d223008d53a031c788596303
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-15 23:56:55 +0100

    comment: "Added image parameter to compute disk and updated tag bindings module to use for_each with instance_numeric_id"

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -113,26 +113,26 @@ module "disk_tag_bindings" {
   application_tag_value = local.application_tag_map[var.application]
 }
 
-module "snapshot_tag_bindings" {
+# module "snapshot_tag_bindings" {
 
-  source = "../../modules/tag-bindings"
+#   source = "../../modules/tag-bindings"
 
-  for_each = module.compute_snapshot.snapshot_numeric_id
+#   for_each = module.compute_snapshot.snapshot_numeric_id
 
-  parent = format(
-    "//compute.googleapis.com/projects/%s/global/snapshots/%s",
-    "106228803995",
-    each.value
-  )
+#   parent = format(
+#     "//compute.googleapis.com/projects/%s/global/snapshots/%s",
+#     "106228803995",
+#     each.value
+#   )
 
-  location = "global"
+#   location = "global"
 
-  environment_tag_value = local.environment_tag_map[var.environment]
+#   environment_tag_value = local.environment_tag_map[var.environment]
 
-  owner_tag_value       = local.owner_tag_map[var.owner]
+#   owner_tag_value       = local.owner_tag_map[var.owner]
 
-  application_tag_value = local.application_tag_map[var.application]
-}
+#   application_tag_value = local.application_tag_map[var.application]
+# }
 
 module "compute_snapshot" {
 
