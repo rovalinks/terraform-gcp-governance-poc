a11fb0d237e09bed0e0d64c6823df013277a5bb8
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-16 00:44:00 +0100

    updated tag binding output to include location and added depends_on to the tag binding resource in dev/main.tf

--- terraform/modules/tag-bindings/outputs.tf
+++ terraform/modules/tag-bindings/outputs.tf
@@ -1,11 +1,24 @@
+# output "environment_binding" {
+#   value = google_tags_location_tag_binding.environment.name
+# }
+
+# output "owner_binding" {
+#   value = google_tags_location_tag_binding.owner.name
+# }
+
+# output "application_binding" {
+#   value = google_tags_location_tag_binding.application.name
+# }
+
+
 output "environment_binding" {
-  value = google_tags_location_tag_binding.environment.name
+  value = var.location == "global" ? one(google_tags_tag_binding.environment_global[*].name) : one(google_tags_location_tag_binding.environment[*].name)
 }
 
 output "owner_binding" {
-  value = google_tags_location_tag_binding.owner.name
+  value = var.location == "global" ? one(google_tags_tag_binding.owner_global[*].name) : one(google_tags_location_tag_binding.owner[*].name)
 }
 
 output "application_binding" {
-  value = google_tags_location_tag_binding.application.name
+  value = var.location == "global" ? one(google_tags_tag_binding.application_global[*].name) : one(google_tags_location_tag_binding.application[*].name)
 }
