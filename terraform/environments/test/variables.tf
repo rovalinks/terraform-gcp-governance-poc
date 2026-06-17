ce177979e918fbbddb102d5d82fbc31a1f8dde77
Rohit <hr@rovalinks.com>
2026-06-17 01:35:21 +0000

    Test Tested and UAT Updated

---
+++ terraform/environments/test/variables.tf
@@ -0,0 +1,47 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+
+  validation {
+    condition = contains(
+      ["dev", "test", "uat", "prod"],
+      lower(var.environment)
+    )
+
+    error_message = "Environment must be one of: dev, test, uat, prod."
+  }
+}
+
+variable "owner" {
+  description = "Resource owner"
+  type        = string
+
+  validation {
+    condition     = length(trimspace(var.owner)) > 0
+    error_message = "Owner is mandatory."
+  }
+}
+
+variable "application" {
+  description = "Application name"
+  type        = string
+
+  validation {
+    condition     = length(trimspace(var.application)) > 0
+    error_message = "Application is mandatory."
+  }
+}
+
+variable "workload_ids" {
+  description = "List of workload identifiers"
+  type        = set(string)
+
+  validation {
+    condition = alltrue([
+      for id in var.workload_ids :
+      can(regex("^\\d{2}$", id))
+    ])
+
+    error_message = "Workload IDs must be two digits (01, 02, 03)."
+  }
+}
