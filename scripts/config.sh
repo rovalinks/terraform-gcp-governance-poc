8022291670d16ac0cf2e2c2afa7f731dce44c718
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-18 01:46:10 +0100

    updated iam deny email templates and added scripts to generate and update deny policies

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-export PROJECT_ID=$(gcloud config get-value project)
-
-export PROJECT_NUMBER=$(gcloud projects describe \
-  $PROJECT_ID \
-  --format="value(projectNumber)")
-
-export ORG_ID=$(gcloud projects describe \
-  $PROJECT_ID \
-  --format="value(parent.id)")
+export GOVERNANCE_ADMIN_EMAIL="rohith555raju@gmail.com"
